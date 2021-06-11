05bb4ea7b3f7fad11928f19aad022b138ce16889
Katherine Yoguez <katherinecarlotayoguez@gmail.com>
2021-06-11 16:39:55 -0400

    fixed bug

--- server.js
+++ server.js
@@ -1,54 +1,57 @@
 const express = require("express");
-const fs = require("fs");
-const notes = require("./db/db.json");
 const path = require("path");
-const uuid = require("uuid");
-const { DH_CHECK_P_NOT_SAFE_PRIME } = require("constants");
+const fs = require("fs");
 
-const app = express();
-var PORT = process.env.PORT || 3001;
+const app = express()
+const PORT = process.env.PORT || 3001;
 
-// Middleware
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(express.static("public"));
 
-//Setting routes for APIs
-//This gets notes saved and joins it in db.json
-app.get("/api/notes", (req, res) => {
-    res.sendFile(path.join(__dirname, "/db/db.json"))
-});
-
-// Post function to add new notes to db.json
-app.post("/api/notes", (req, res) => {
-    const notes = JSON.parse(fs.readFileSync("./db/db.json"));
-    const newNotes = req.body;
-    newNotes.id = uuid.v4();
-    notes.push(newNotes);
-    fs.writeFileSync("./db/db.json", JSON.stringify(notes))
-    res.json(notes);
-});
-
-//used for deleting notes
-app.delete("/api/notes/:id", (req, res) => {
-    const notes = JSON.parse(fs.readFileSync("./db/db.json"));
-    const delNote = notes.filter((rmvNote) => rmvNote.id !== req.params.id);
-    fs.writeFileSync("./db/db.json", JSON.stringify(delNote));
-    res.json(delNote);
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/notes.html"))
 })
 
+app.get("/api/notes", (req, res) => {
+    fs.readFile("./db/db.json", (err, data) => {
+        if(err) throw err;
+        let dataEntry = JSON.parse(data)
+        res.json(dataEntry)
+    })
+})
 
-//HTML calls
-//calls home page
-app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname, "/public/index.html"));
-});
-//call for notes.html
-app.get("/notes", function (req, res) {
-    res.sendFile(path.join(__dirname, "/public/notes.html"));
-});
+app.post("/api/notes", (req, res) => {
+    fs.readFile("./db/db.json", (err, data) => {
+        if(err) throw err;
+        let arrOfNotes = JSON.parse(data)
+        let dataNewNote = req.body
+        dataNewNote["id"] = Math.floor(Math.random()*1)
+        arrOfNotes.push(req.body)
+        fs.writeFile("./db/db.json", JSON.stringify(arrOfNotes), (err) => {
+            if(err) throw err;
+            res.json(req.body)
+        })
+    })
+})
 
-//Start listen
-app.listen(PORT, function () {
-    console.log("App listening on PORT: " + PORT);
-});
+app.delete("/api/notes/:id", (req, res) => {
+    let idToDelete = req.params.id
+    fs.readFile("./db/db.json", (err, data) => {
+        if(err) throw err;
+        let arrOfNotes = JSON.parse(data)
+        for(i=0; i < arrOfNotes.length; i++){
+            if(arrOfNotes[i].id == idToDelete){
+                arrOfNotes.splice(i, 1)
+            }
+        }
+        fs.writeFile("./db/db.json", JSON.stringify(arrOfNotes), (err) => {
+            if(err) throw err;
+            res.json(req.body)
+        })
+    })
+})
+
+app.listen(PORT, ()=>{
+    console.log("working")
+})
